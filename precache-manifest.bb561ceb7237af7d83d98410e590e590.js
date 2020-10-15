5a9db8c7057288ab01552b4d41a46cafceaeb0ed
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-10-16 03:14:09 +0530

    Updates

---
+++ precache-manifest.bb561ceb7237af7d83d98410e590e590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84e41a8178c323b7b1b6abb6d6e3d2c",
+    "url": "/AffinityAnswers-Ecommerce/index.html"
+  },
+  {
+    "revision": "e97a8268762897f926bb",
+    "url": "/AffinityAnswers-Ecommerce/static/css/main.9aa6ef56.chunk.css"
+  },
+  {
+    "revision": "4be06e27c64df1e95710",
+    "url": "/AffinityAnswers-Ecommerce/static/js/2.89a0499a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AffinityAnswers-Ecommerce/static/js/2.89a0499a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97a8268762897f926bb",
+    "url": "/AffinityAnswers-Ecommerce/static/js/main.c2ca5d88.chunk.js"
+  },
+  {
+    "revision": "46b510d4c0dc8965536f",
+    "url": "/AffinityAnswers-Ecommerce/static/js/runtime-main.38abfe95.js"
+  }
+]);
