2261580c8588e8bd2cc956055210f175cc1124ad
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-10-23 10:33:45 +0530

    Update card header

--- src/components/CartPanel.js
+++ src/components/CartPanel.js
@@ -2,32 +2,23 @@ import React from 'react';
 import CartItem from "./CartItem";
 
 function CartPanel(props) {
-    if (!props.cartItems.length) {
-        return (
-            <div id="cart" className="cart-panel">
+
+    var numberOfItems = props.cartItems.length;
+    return (
+        <div id="cart" className="cart-panel">
+            <div className="cart-header">
                 <h2>My Cart</h2>
                 <h2>Total: {props.cartPrice}</h2>
-                <div onClick={() => { props.closeCart(); }} className="close-cart">❌</div>
-                <hr />
             </div>
-        )
-    } else {
-        return (
-            <div id="cart" className="cart-panel">
-                <div className="cart-header">
-                    <h2>My Cart</h2>
-                    <h2>Total: {props.cartPrice}</h2>
-                </div>
-                <div onClick={() => { props.closeCart(); }} className="close-cart">❌</div>
-                <hr />
-                <div className="cart-items-container">
-                    {props.cartItems.map(item => {
-                        return (<CartItem incDecItem={props.incDecItem} key={item.key} item={item} removeItemFromCart={props.removeItemFromCart} />)
-                    })}
-                </div>
-            </div>
-        )
-    }
+            <div onClick={() => { props.closeCart(); }} className="close-cart">❌</div>
+            <hr />
+            {(numberOfItems != 0) && <div className="cart-items-container">
+                {props.cartItems.map(item => {
+                    return (<CartItem incDecItem={props.incDecItem} key={item.key} item={item} removeItemFromCart={props.removeItemFromCart} />)
+                })}
+            </div>}
+        </div>
+    )
 }
 
 export default CartPanel;
